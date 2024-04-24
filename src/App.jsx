692c423fabf15a3f7f1a27466ceaf83da446635f
Jai Bhullar <81278507+JaiBh@users.noreply.github.com>
2024-04-24 13:17:47 +0100

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+import ColorList from "./components/ColorList";
+import Form from "./components/Form";
+import Values from "values.js";
+import { ToastContainer, toast } from "react-toastify";
+
 const App = () => {
-  return <h2>Color Generator Starter</h2>;
+  const [colors, setColors] = useState(new Values("#f15025").all(10));
+
+  const addColor = (color) => {
+    try {
+      const newColor = new Values(color).all(10);
+      setColors(newColor);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+  return (
+    <main>
+      <Form addColor={addColor}></Form>
+      <ColorList colors={colors}></ColorList>
+      <ToastContainer position="top-center"></ToastContainer>
+    </main>
+  );
 };
 export default App;
